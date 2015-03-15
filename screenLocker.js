95235a76738b76b31943da600c9a573e99704c88
Fredrik Källström <fredrikkallstrom@FKMBP.local>
2015-03-15 17:10:40 +0100

    Script to unlock screen (OS X) added

--- screenLocker.js
+++ screenLocker.js
@@ -21,10 +21,29 @@ var screenLockScript =
 			end tell\n\
 		end tell';
 
+var screenUnlockScript =
+		'tell application "Terminal"\n\
+			do shell script "caffeinate -u -t 1"\n\
+			end tell\n\
+			delay 0.5\n\
+		tell application "System Events" to tell process "loginwindow"\n\
+			activate\n\
+			delay 0.2\n\
+            tell window "Login Panel"\n\
+                keystroke "'+ config.password +'"\n\
+                keystroke return\n\
+            end tell\n\
+		end tell';
+
 self.lockScreen = function() {
 	applescript.execString(screenLockScript);
 }
 
+self.unlockScreen = function() {
+	applescript.execString(screenUnlockScript);
+}
+
 module.exports = {
-	lockScreen : self.lockScreen
+	lockScreen : self.lockScreen,
+	unlockScreen: self.unlockScreen
 };
