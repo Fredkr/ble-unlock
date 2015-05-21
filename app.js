bf54e10c309f29466c351087105c8119b51cfd63
Fredkr <info@fredrikkallstrom.com>
2015-05-21 14:03:45 +0200

    Only scanning for beacons set in config

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var bleScan = require('./bleScanner');
+Array.prototype.contains = function(element){
+    return this.indexOf(element) > -1;
+};
+
+var bleScan =require('./bleScanner');
 bleScan.scan();
+
--- bleScanner.js
+++ bleScanner.js
@@ -5,22 +5,23 @@ var bleacon = require('bleacon'),
 	util = require('util');
 
 exports.scan = function() {
-	bleacon.startScanning();
+	var uuids = config.lockUUIDs.concat( config.unlockUUIDs );
+	bleacon.startScanning(uuids);
 	logger.info('scan started');
 	var self = this;
 	self.timeOut = false;
 
 	bleacon.on('discover', function(bleacon) {
-		console.log(util.inspect(bleacon, {showHidden: false, depth: null}));
+		//console.log(util.inspect(bleacon, {showHidden: false, depth: null}));
 
 		if(!self.timeOut){
 			self.timeOut = true;
 			screenSaver.screenIsActive(function(screenIsActive) {
-				if(screenIsActive && bleacon.uuid === '0d65410849314945b4120615bdb895b5'){
+				if(screenIsActive && config.lockUUIDs.contains(bleacon.uuid)){
 					screenSaver.lockScreen();
 				}
-				else if(!screenIsActive && bleacon.uuid === 'bdbed62785c14e33a755ff3e956262e4'){
-					screenSaver.unlockScreen();			
+				else if(!screenIsActive && config.unlockUUIDs.contains(bleacon.uuid)){
+					screenSaver.unlockScreen();	
 				}
 
 				setTimeout(function () {self.timeOut = false;}, 500);
